b380e53c230b3f2bdc5b9171e30e47e00ba4921d
jiaxinying <jiaxinying@91taogu.com>
2018-05-09 10:52:07 +0800

    swiper

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -3,7 +3,7 @@
         <div class="search-bar">
             <van-row>
                 <van-col span="3">
-                    <img :src="locationIcon" width="80%" class="location-icon"/>
+                    <img :src="locationIcon" width="80%" class="location-icon" />
                 </van-col>
                 <van-col span="16">
                     <input type="text" class="search-input" />
@@ -16,19 +16,19 @@
         <!--swiper area-->
         <div class="swiper-area">
             <van-swipe :autoplay="1000">
-                <van-swipe-item v-for="( banner ,index) in bannerPicArray" :key="index" >
-                    <img v-lazy="banner.image" width="100%"/>
+                <van-swipe-item v-for="( banner ,index) in bannerPicArray" :key="index">
+                    <img v-lazy="banner.image" width="100%" />
                 </van-swipe-item>
             </van-swipe>
-        </div>  
+        </div>
         <!--type bar-->
         <div class="type-bar">
             <div v-for="(cate,index) in category" :key="index">
                 <img v-lazy="cate.image" width="90%">
                 <span>{{cate.mallCategoryName}}</span>
-
+    
             </div>
-        </div>  
+        </div>
         <!--adbanner area-->
         <div>
             <img v-lazy="adBanner" width="100%" />
@@ -40,117 +40,136 @@
             </div>
             <div class="recommend-body">
                 <swiper :options="swiperOption">
-                   <swiper-slide v-for="(item,index) in recommendGoods " :key="index" >
-                       <div class="recommend-item">
-                           <img :src="item.image" width="80%">
-                           <div>{{item.goodsName}}</div>
-                           <div>￥{{item.price}}(￥{{item.mallPrice}})</div>
-                       </div>
-                   </swiper-slide> 
+                    <swiper-slide v-for="(item,index) in recommendGoods " :key="index">
+                        <div class="recommend-item">
+                            <img :src="item.image" width="80%">
+                            <div>{{item.goodsName}}</div>
+                            <div>￥{{item.price}}(￥{{item.mallPrice}})</div>
+                        </div>
+                    </swiper-slide>
                 </swiper>
             </div>
         </div>
+        <!--swiper示例-->
+        <swiperDefault></swiperDefault>
     </div>
 </template>
 
 <script>
     import axios from 'axios'
     import 'swiper/dist/css/swiper.css'
-    import {swiper , swiperSlide} from 'vue-awesome-swiper'
+    import {
+        swiper,
+        swiperSlide
+    } from 'vue-awesome-swiper'
+    import swiperDefault from '../swiper/swiperDefault'
     export default {
         data() {
             return {
-                swiperOption:{
-                    slidesPerView:3
+                swiperOption: {
+                    slidesPerView: 3
                 },
                 msg: 'Shopping Mall',
                 locationIcon: require('../../assets/images/location.png'),
-                bannerPicArray:[],
-                category:[],
-                adBanner:'',
-                recommendGoods:[],
+                bannerPicArray: [],
+                category: [],
+                adBanner: '',
+                recommendGoods: [],
             }
         },
-        components:{swiper,swiperSlide},
-        created(){
+        components: {
+            swiper,
+            swiperSlide,
+            swiperDefault
+        },
+        created() {
             axios({
-                url:'https://www.easy-mock.com/mock/5ae993753ecdf019c288e529/smileVue/index',
-                method:'get',
-            })
-            .then(response=>{
-                console.log(response)
-                if(response.status==200){
-                    this.category=response.data.data.category;
-                    this.adBanner = response.data.data.advertesPicture.PICTURE_ADDRESS;
-                    this.bannerPicArray= response.data.data.slides;
-                    this.recommendGoods = response.data.data.recommend;
-                }
-            })
-            .catch(error=>{
-                console.log(error)
-            })
+                    url: 'https://www.easy-mock.com/mock/5ae993753ecdf019c288e529/smileVue/index',
+                    method: 'get',
+                })
+                .then(response => {
+                    console.log(response)
+                    if (response.status == 200) {
+                        this.category = response.data.data.category;
+                        this.adBanner = response.data.data.advertesPicture.PICTURE_ADDRESS;
+                        this.bannerPicArray = response.data.data.slides;
+                        this.recommendGoods = response.data.data.recommend;
+                    }
+                })
+                .catch(error => {
+                    console.log(error)
+                })
         }
-       
+    
     }
 </script>
 
 <style scoped>
-    .search-bar{
-        height:2.2rem;
+    .search-bar {
+        height: 2.2rem;
         background-color: #e5017d;
         line-height: 2.2rem;
         overflow: hidden;
     }
-    .search-input{
-        width:100%;
+    
+    .search-input {
+        width: 100%;
         height: 1.3rem;
-        border-top:0px;
-        border-left:0px;
-        border-right:0px;
-        border-bottom:1px solid #fff !important;
+        border-top: 0px;
+        border-left: 0px;
+        border-right: 0px;
+        border-bottom: 1px solid #fff !important;
         background-color: #e5017d;
-        color:#fff;
+        color: #fff;
     }
-    .location-icon{
-        padding-top:.2rem;
-        padding-left:.3rem;
+    
+    .location-icon {
+        padding-top: .2rem;
+        padding-left: .3rem;
     }
-    .swiper-area{
-        clear:both;
-        max-height:15rem;
+    
+    .swiper-area {
+        clear: both;
+        max-height: 15rem;
         overflow: hidden;
     }
-    .type-bar{
+    
+    .type-bar {
         background-color: #fff;
-        margin:0 .3rem .3rem .3rem;
+        margin: 0 .3rem .3rem .3rem;
         border-radius: .3rem;
-        font-size:14px;
-        display:flex;
-        flex-direction:row;
-        flex-wrap:nowrap;
+        font-size: 14px;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
     }
-    .type-bar div{
-        padding:.3rem;
-        font-size:12px;
+    
+    .type-bar div {
+        padding: .3rem;
+        font-size: 12px;
         text-align: center;
     }
-    .recommend-area{
+    
+    .recommend-area {
         background-color: #fff;
-        margin-top:.3rem;
+        margin-top: .3rem;
     }
-    .recommend-title{
-        border-bottom:1px solid #eee;
-        font-size:14px;
-        padding:.2rem;
-        color:#e5017d;
+    
+    .recommend-title {
+        border-bottom: 1px solid #eee;
+        font-size: 14px;
+        padding: .2rem;
+        color: #e5017d;
     }
-    .recommend-body{
-        border-bottom:1px solid #eee;
+    
+    .recommend-body {
+        border-bottom: 1px solid #eee;
     }
-    .recommend-item{
-        width:99%;
-        border-right:1px solid #eee;
-        font-size:12px;
+    
+    .recommend-item {
+        width: 99%;
+        border-right: 1px solid #eee;
+        font-size: 12px;
         text-align: center;
     }
 </style>
